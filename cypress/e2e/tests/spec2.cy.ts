95c9e6145d41954c880d763a49b499721d0410a6
Sergey Chevychelov <qa.schevychelov@gmail.com>
2022-07-31 11:42:43 +0300

    добавил либу для работы с датами как в джаве

--- cypress/e2e/tests/spec2.cy.ts
+++ cypress/e2e/tests/spec2.cy.ts
@@ -2,7 +2,6 @@ import { burgerStep } from '../steps/burgerStep'
 import * as policy from '../../fixtures/confidentPolicy.json'
 import * as oferta from '../../fixtures/oferta.json'
 import * as returnPolicy from '../../fixtures/returnPolicy.json'
-
 describe('SMMTOUCH.TECH - проверка соглашений', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -15,12 +14,13 @@ describe('SMMTOUCH.TECH - проверка соглашений', () => {
   })
   it('публичная оферта', () => {
     burgerStep.clickLink("Публичная оферта")
-    .checkAnyTextIsVisible(oferta.ttl)
-    .checkAnyTextIsVisible(oferta.body)
+      .checkAnyTextIsVisible(oferta.ttl)
+      .checkAnyTextIsVisible(oferta.body)
   })
   it('политика возврата', () => {
     burgerStep.clickLink("Политика возврата")
-    .checkAnyTextIsVisible(returnPolicy.ttl)
-    .checkAnyTextIsVisible(returnPolicy.body)
+      .checkAnyTextIsVisible(returnPolicy.ttl)
+      .checkAnyTextIsVisible(returnPolicy.body)
+
   })
 })
